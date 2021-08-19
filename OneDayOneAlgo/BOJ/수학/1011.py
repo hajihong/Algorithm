a05144829a0bb8eeabb58140b9b75bf3c33bd348
hajihong <hajihong96@gmail.com>
2021-08-19 17:48:23 +0900

    풀이

---
+++ OneDayOneAlgo/BOJ/수학/1011.py
@@ -0,0 +1,15 @@
+# FLY TO THE MOON
+t = int(input())
+
+for i in range(t):
+    x,y = map(int, input().split())
+    distance = y-x
+    count = 0
+    move = 1
+    move_plus = 0
+    while move_plus < distance:
+        count += 1
+        move_plus += move
+        if count % 2 == 0:
+            move += 1
+    print(count)
